3dd4386ef2e16a63abb9e790750e883a7e563a09
kientt6602-ubuntu <kientt6602@co-well.vn>
2022-08-30 17:36:46 +0700

    Xoa code thua va file thua

--- src/feature/Photo/pages/MainPage/index.jsx
+++ src/feature/Photo/pages/MainPage/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Banner from '../../../../components/Banner/Banner';
-import Images from '../../../../constants/image';
+import Banner from 'components/Banner/Banner';
+import Images from 'constants/image';
 import { Container } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
